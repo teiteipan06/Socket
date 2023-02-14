3adc48230929a2f2b5525704e7224efd62ba5eec
teiteipan06 <y7321543@gmail.com>
2023-02-14 23:11:38 +0900

    setting of server

--- SocketServer.java
+++ SocketServer.java
@@ -1,5 +1,20 @@
-package Socket;
+import java.net.ServerSocket;
+import java.net.Socket;
 
-public class SocketServer {
-  
-}
+public class SocketServer extends java.lang.Thread {
+
+  private boolean OutServer = false;
+  private ServerSocket server;
+  private final int ServerPort = 8765;// 要監控的port
+
+  public SocketServer() {
+    try {
+      server = new ServerSocket(ServerPort);
+
+    } catch (java.io.IOException e) {
+        System.out.println("Socket啟動有問題 !");
+        System.out.println("IOException :" + e.toString());
+    }
+  }
+
+    
