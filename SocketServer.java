0af7c77ef26decd31745d5167eb77864a8e8dcb1
teiteipan06 <y7321543@gmail.com>
2023-02-15 20:45:26 +0900

    setting of server

--- SocketServer.java
+++ SocketServer.java
@@ -17,4 +17,47 @@ public class SocketServer extends java.lang.Thread {
     }
   }
 
+  public void run() {
+    Socket socket;
+    java.io.BufferedInputStream in;
+
+    System.out.println("伺服器已啟動 !");
+    while (!OutServer) {
+      socket = null;
+      try {
+          synchronized (server) {
+            socket = server.accept();
+          }
+          System.out.println("取得連線 : InetAddress = "
+              + socket.getInetAddress());
+          // TimeOut時間
+          socket.setSoTimeout(15000);
+
+          in = new java.io.BufferedInputStream(socket.getInputStream());
+          byte[] b = new byte[1024];
+          String data = "";
+          int length;
+          while ((length = in.read(b)) == 0)// &lt;=0的話就是結束了
+          {
+          　  data += new String(b, 0, length);
+          }
+
+          System.out.println("我取得的值:" + data);
+          in.close();
+          in = null;
+          socket.close();
+
+        } catch (java.io.IOException e) {
+            System.out.println("Socket連線有問題 !");
+            System.out.println("IOException :" + e.toString());
+        }
+
+    }
+  }
+
+  public static void main(String args[]) {
+    (new SocketServer()).start();
+  }
+}
+
     
