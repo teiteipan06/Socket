fa4acc2f7fd31b91fc358580f5cbcdc665f4baf9
teiteipan06 <y7321543@gmail.com>
2023-02-16 22:59:00 +0900

    create client java

---
+++ Client.java
@@ -0,0 +1,15 @@
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.io.BufferedOutputStream;
+
+public class SocketClient {
+    private String address = "127.0.0.1"; // 連線的ip
+    private int port = 8765;// 連線的port
+
+    public SocketClient() {
+
+        Socket client = new Socket();
+
+    
+    }
+}
