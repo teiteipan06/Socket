1f93595e97da8217190bfdf65c1b653dbc002c94
teiteipan06 <y7321543@gmail.com>
2023-02-18 22:46:04 +0900

    update client.java

--- Client.java
+++ Client.java
@@ -3,13 +3,32 @@ import java.net.Socket;
 import java.io.BufferedOutputStream;
 
 public class SocketClient {
-    private String address = "127.0.0.1"; // 連線的ip
-    private int port = 8765;// 連線的port
+  private String address = "127.0.0.1"; // 連線的ip
+  private int port = 8765;// 連線的port
 
-    public SocketClient() {
+  public SocketClient() {
 
-        Socket client = new Socket();
+    Socket client = new Socket();
+    InetSocketAddress isa = new InetSocketAddress(this.address, this.port);
+    try {
+        client.connect(isa, 10000);
+        BufferedOutputStream out = new BufferedOutputStream(client
+                 .getOutputStream());
+        // 送出字串
+        out.write("Send From Client ".getBytes());
+        out.flush();
+        out.close();
+        out = null;
+        client.close();
+        client = null;
 
-    
-    }
+      } catch (java.io.IOException e) {
+          System.out.println("Socket連線有問題 !");
+          System.out.println("IOException :" + e.toString());
+      }
+  }
+
+  public static void main(String args[]) {
+    new SocketClient();
+  }
 }
